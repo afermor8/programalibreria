b457a48f3d3a4a7512b22db83e2dc169d4a3264a
afermor8 <ara.fer.mor@gmail.com>
2022-06-11 13:59:55 +0200

    He añadido la plantilla html y he creado app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, abort
+import os
+import json
+
+app = Flask (__name__)
+
+f= open("books.json")
+biblioteca=json.load(f)
+f.close()
+
+@app.route('/',methods=["GET","POST"])
+def index():
+    return render_template("index.html",libros=datos)
+
+@app.route('/libro/<isbn>',methods=["GET","POST"])
+def libro(isbn):
+    for book in datos:
+        if "isbn" in book.keys() and isbn == book["isbn"]:
+            return render_template("libro.html", libro=book)
+    abort(404)
+
+@app.route('/categoria/<categoria>',)
+def categoria(categoria):
+    for category in datos:
+        if "categories" in category.keys() and categoria in category["categories"]:
+            return render_template("categoria.html", libros=datos, categoria=categoria)
+    abort(404)
+
+app.run('0.0.0.0' ,debug=False)
